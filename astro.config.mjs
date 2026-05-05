aecfd56fb73a6041cd93ee8f92b64c4b4d522218
Valentina Ramírez <anaj0111719@gmail.com>
2026-05-05 16:25:59 -0500

    [CREATING] --components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
+import icon from 'astro-icon'
 
 export default defineConfig({
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [tailwind({ applyBaseStyles: false }), icon()],
   site: 'https://terracoreapp.co',
 })
