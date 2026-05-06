66accb7a6a9d7a525f3e8a859e9f2d3697a16ed3
Valentina Ramírez <anaj0111719@gmail.com>
2026-05-06 12:45:07 -0500

    [UPDATING] --seo-a11y-performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'hybrid',
   adapter: node({ mode: 'standalone' }),
-  integrations: [tailwind({ applyBaseStyles: false }), icon()],
+  integrations: [tailwind({ applyBaseStyles: false }), icon(), sitemap()],
   site: 'https://terracoreapp.co',
 });
