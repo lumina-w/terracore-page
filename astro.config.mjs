8cb49a120363de1cdc91e4cb59d0a999f817388a
Valentina Ramírez <anaj0111719@gmail.com>
2026-05-06 15:34:53 -0500

    landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import netlify from '@astrojs/netlify';
-import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   output: 'hybrid',
   adapter: netlify(),
-  integrations: [tailwind({ applyBaseStyles: false }), icon(), sitemap()],
+  integrations: [tailwind({ applyBaseStyles: false }), icon()],
   site: 'https://terracoreapp.co',
   devToolbar: { enabled: false },
 });
